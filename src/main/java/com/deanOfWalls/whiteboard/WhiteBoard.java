0a222e26fef8b61a43127dfa37eb6df3066adce9
deanOfWalls <deanofwalls@gmail.com>
2023-09-06 05:19:11 -0400

    Initial commit

---
+++ src/main/java/com/deanOfWalls/whiteboard/WhiteBoard.java
@@ -0,0 +1,13 @@
+package com.deanOfWalls.whiteboard;
+
+public class WhiteBoard {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String testMethod (){
+        return null;
+    }
+
+}
---
+++ src/main/test/java/com.deanOfWalls.whiteboard/WhiteBoardTest.java
@@ -0,0 +1,4 @@
+package com.deanOfWalls.whiteboard;
+
+public class WhiteBoardTest {
+}
